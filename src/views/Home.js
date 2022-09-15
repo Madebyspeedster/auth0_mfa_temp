dcffe646e88a7db6c38a162a8a07639d7bb65ac7
unknown <byspeedster@gmail.com>
2022-09-15 16:40:40 +0300

    feat: Added draft mfa

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,30 +1,79 @@
-import React, {Fragment, useEffect} from "react";
-
-import Hero from "../components/Hero";
-import Content from "../components/Content";
+import React, {Fragment, useCallback, useState} from "react";
 import {useAuth0} from "@auth0/auth0-react";
+import {Button} from "reactstrap";
+
+
+
+const getUserMeta = (user) => {
+    return fetch(`https://helpmebear.herokuapp.com/level-field/get-user-data?userId=${encodeURI(`${user.sub}`)}`,  {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then(result => result.json())
+        .then(user => user)
+        .catch(e => {
+            console.log(e)
+        })
+}
+
+
+
 
 const Home = () => {
-    const {getAccessTokenWithPopup, error, user, getAccessTokenSilently} = useAuth0();
+    const {getAccessTokenWithPopup, user} = useAuth0();
+    const [loading, setIsLoading] = useState(false);
+    const [transactionDone, setTransactionIsDone] = useState(false);
+    const [shouldGenerateMfa, setShouldGenerateMFA] = useState(false);
 
-    useEffect(() => {
-        getAccessTokenSilently().then(res => {
-            console.log(res);
-        }).catch(e => {
-            console.log('Error', e)
+    const commitTransaction = useCallback(async () => {
+        setIsLoading(true);
+        const userInfo = await getUserMeta(user);
+        if(!userInfo?.enrolments?.length) {
+            setShouldGenerateMFA(true);
+            setIsLoading(false);
+            return;
+        }
+        getAccessTokenWithPopup({confirmTransaction: true})
+            .then(() => setTransactionIsDone(true))
+            .catch(e => console.log({e}))
+        setIsLoading(false);
+    }, [getAccessTokenWithPopup, user]);
+
+    const generateMFA = useCallback((user) => {
+        setIsLoading(true);
+        return fetch(`https://helpmebear.herokuapp.com/level-field/generate-mfa?userId=${encodeURI(`${user.sub}`)}`,  {
+            headers: {
+                "Content-Type": "application/json",
+            },
         })
-    }, [error, user])
+            .then(result => result.json())
+            .then(({ticket}) => {
+                console.log(ticket)
+                if(ticket.ticket_url){
+                    setShouldGenerateMFA(false);
+                    window.open(ticket.ticket_url,  "mozillaWindow", "popup")
+                }
+            })
+            .catch(e => {
+                console.log(e)
+            })
+            .finally(() => {setIsLoading(false)})
+    }, [])
+
     return (
         <Fragment>
-            <Hero />
-            {user && (<button onClick={
-                () => getAccessTokenWithPopup({ignoreCache: true, confirmTransaction: true})
-                    .then(res => console.log({res}))
-                    .catch(e => console.log({e}))}>
-                confirm transaction
-            </button>)}
-            <hr />
-            <Content />
+            {user && (<Button onClick={commitTransaction}> {loading ? 'wait' : 'confirm transaction' }</Button>)}
+            {transactionDone && (
+                <h3 style={{margin: 15}}>All good! 🥳</h3>
+            )}
+            {shouldGenerateMfa && (
+               <>
+                   <h3 style={{margin: 15}}>Hey, in order to proceed we want you to setup MFA 📱</h3>
+                   <Button onClick={() => generateMFA(user)}>{loading ? 'wait': 'Setup MFA'}</Button>
+               </>
+            )}
+            <hr/>
         </Fragment>
     )
 };
